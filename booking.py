844d1b724a815b33280012b987754557e96b4d17
hrantgartley <doyouevenjump@gmail.com>
2024-08-08 09:20:08 -0500

    documented code better

--- booking.py
+++ booking.py
@@ -29,6 +29,9 @@ class Booking:
         self.payment_id = None
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the booking object.
+        """
         return (
             f"Booking ID: {self.booking_id}\n"
             f"Status: {self.status}\n"
@@ -36,27 +39,73 @@ class Booking:
             f"Payment ID: {self.payment_id}\n"
         )
 
-    def get_booking_id(self) -> str:
+    def get_booking_id(self) -> str | None:
+        """
+        Returns the booking ID.
+
+        :return: The booking ID as a string, or None if no booking ID is set.
+        """
         return self.booking_id
 
     def get_status(self) -> str:
+        """
+        Returns the status of the booking.
+
+        :return: The status of the booking.
+        :rtype: str
+        """
         return self.status
 
     def get_payment_status(self) -> str:
+        """
+        Returns the payment status of the booking.
+
+        :return: A string representing the payment status.
+        """
         return self.payment_status
 
     def get_payment_id(self) -> int:
+        """
+        Returns the payment ID associated with the booking.
+
+        :return: The payment ID.
+        :rtype: int
+        """
         return self.payment_id
 
     def generate_booking_id(self) -> str:
+        """
+        Generates a unique booking ID.
+
+        Returns:
+            str: The generated booking ID.
+        """
         return "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
     def create_booking(self, status, payment_status, payment_id) -> None:
+        """
+        Creates a booking with the given status, payment status, and payment ID.
+
+        Parameters:
+        - status (str): The status of the booking.
+        - payment_status (str): The payment status of the booking.
+        - payment_id (int): The ID of the payment associated with the booking.
+
+        Returns:
+        None
+        """
         self.status = status
         self.payment_status = payment_status
         self.payment_id = payment_id
 
     def check_booking_object(self, booking: "Booking") -> bool:
+        """
+        Check if the given booking object matches the current object.
+        Parameters:
+            booking (Booking): The booking object to compare with.
+        Returns:
+            bool: True if the booking object matches the current object, False otherwise.
+        """
         class_vars = vars(self)
         booking_vars = vars(booking)
 
